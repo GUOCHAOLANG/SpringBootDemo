71fbdedeb700849264e060b44f7fb60eee1c45a5
chaolang.guo@ifchange.com <chaolang.guo@ifchange.com>
2020-04-08 11:20:04 +0800

    刷题

---
+++ domebaseweb/src/test/java/com/gcl/example/domebaseweb/One.java
@@ -0,0 +1,31 @@
+package com.gcl.example.domebaseweb;
+
+/**
+ * gcl
+ * 2020/2/21 16:10
+ */
+public class One {
+    private Integer a;
+    private Boolean t;
+
+    public One(Integer a, Boolean t) {
+        this.a = a;
+        this.t = t;
+    }
+
+    public Integer getA() {
+        return a;
+    }
+
+    public void setA(Integer a) {
+        this.a = a;
+    }
+
+    public Boolean getT() {
+        return t;
+    }
+
+    public void setT(Boolean t) {
+        this.t = t;
+    }
+}
